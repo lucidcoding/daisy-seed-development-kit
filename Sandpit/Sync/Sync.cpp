61d1ae19033410306d8bec4a322d48459bd2fb96
Paul Davies <paul.davies@cps.gov.uk>
2024-03-31 11:10:26 +0100

    Sandpitting sync

--- Sandpit/Sync/Sync.cpp
+++ Sandpit/Sync/Sync.cpp
@@ -16,19 +16,44 @@ int main(void)
 {
     hardware.Configure();
     hardware.Init();
+    hardware.StartLog(false);
     float sampleRate = hardware.AudioSampleRate();
     hardware.StartAudio(AudioCallback);
     dsy_gpio gate_output;
     gate_output.pin = hardware.GetPin(24);
     gate_output.mode = DSY_GPIO_MODE_OUTPUT_PP;
     gate_output.pull =  DSY_GPIO_PULLUP; //DSY_GPIO_NOPULL;
+
+    
+    dsy_gpio gate_input;
+    gate_input.pin = hardware.GetPin(25);
+    gate_input.mode = DSY_GPIO_MODE_INPUT;
+    gate_input.pull =DSY_GPIO_PULLUP;//  DSY_GPIO_PULLUP; //DSY_GPIO_NOPULL;
+
     dsy_gpio_init(&gate_output);
+    dsy_gpio_init(&gate_input);
+
+    bool lastRead = true;
+    bool ledState = false;
 
     while (1)
     {
-        dsy_gpio_write(&gate_output, true);  // set high
+        /*dsy_gpio_write(&gate_output, true);  // set high
         System::Delay(15);
         dsy_gpio_write(&gate_output, false);  // set high
-        System::Delay(200);
+        System::Delay(200);*/
+        //dsy_gpio_read(&gate_input);
+        bool currentRead = dsy_gpio_read(&gate_input);
+        //hardware.PrintLine("currentRead: %d", currentRead);
+
+        if(lastRead == true && currentRead == false)
+        {
+            ledState = !ledState;
+        }
+
+        lastRead = currentRead;
+
+        hardware.SetLed(ledState);
+        
     }
 }
